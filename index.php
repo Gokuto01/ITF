e0cef0740a5a820d01c1a5f460104eb122954259
Gokuto01 <73678927+Gokuto01@users.noreply.github.com>
2020-10-30 10:44:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <html>
     <head>
-        <title>My name is Jeeraphat Sakulsubwattana</title>
+        <title>TETE Dog & Cat</title>
     </head>
     <body>
+         <h1>My name is Jeeraphat Sakulsubwattana</h1>
+        <p>My nickname is Golf</p>
+        <p>My StudentID is 63070021</p>
+        <br>
+        
         <h1>This is Nekomata Okayu</h1>
         <p>Shi is cat</p>
         <img src="cat.jpg">
