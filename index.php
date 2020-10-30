ffd2ce82d1ba223a5dda62c2d1adda55faeeafd0
Gokuto01 <73678927+Gokuto01@users.noreply.github.com>
2020-10-30 10:17:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,5 +2,4 @@
 echo "<h2>My Name is Jeeraphat Sakulsubwattana</h2>";
 echo "My Nickname is Golf<br>";
 echo "Student ID: 63070021<br>";
-echo <a href="https://gokuto.azurewebsites.net/index2.html">This is a Link</a>
 ?>
